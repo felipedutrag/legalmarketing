4d766aa10063ebb071457b75d9076efc33c21388
felipedutrag <felipedutra@outlook.com>
2026-09-12 20:25:51 -0300

    feat: adicionar Reddit Pixel ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script id="gtag-init" strategy="afterInteractive">
           {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-18263949464');`}
         </Script>
+        <Script id="reddit-pixel" strategy="afterInteractive">
+          {`!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=a2_jo82q4y3vyus",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_jo82q4y3vyus');rdt('track', 'PageVisit');`}
+        </Script>
       </body>
     </html>
   )
